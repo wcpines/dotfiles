af347bde3445e169840d4f63b63f2ea1dbc2fc9d
Colby Pines <wcpines@gmail.com>
2017-09-12 15:16:55 -0700

    WIP

--- setup.sh
+++ setup.sh
@@ -1,10 +1,31 @@
+# !/bin/bash
+
 # WORK IN PROGRESS
 # Goal: Easily set-up new mac, little manual installs and (re-)config
 
-# TODO: 
-# Create gitconfig
-# Create SSH key-pair
-# Add SSH keys to github!
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until setup has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+COMPUTER_NAME="colby"
+MY_EMAIL="wcpines@gmail.com"
+
+# Name computer
+echo "Naming comp"
+
+sudo scutil --set ComputerName "$COMPUTER_NAME"
+sudo scutil --set HostName "$COMPUTER_NAME"
+sudo scutil --set LocalHostName "$COMPUTER_NAME"
+
+#Create SSH keypair
+
+echo "creating your SSH keys"
+ssh-keygen -f "$HOME/.ssh/id_rsa" -t rsa -b 4096 -C "$MY_EMAIL"
+
+# move to home dir
+cd
 
 # Check for Homebrew,
 # Install if we don't have it
@@ -25,23 +46,34 @@ brew tap Homebrew/bundle
 echo "Install mas-cli..."
 brew install mas
 
-# run symlink script
-sh symlink_script.sh
+# Sign into the app store
+printf "Sign into the Mac App Store\n\n ===>"
+mas signin wcpines@gmail.com
 
-# install Brewfile
+# run symlink script
+echo "Running symlinker!"
+sh ~/dotfiles/symlink_script.sh
+
+# install apps and tools in Brewfile
 brew bundle
 
-# create nvim config:
+# create nvim config
 # https://neovim.io/doc/user/nvim.html#nvim-from-vim
-# To start the transition, create ~/.config/nvim/init.vim with these contents:
-
-#     set runtimepath^=~/.vim runtimepath+=~/.vim/after
-#     let &packpath = &runtimepath
-#     source ~/.vimrc
-
-# ^ plugins should automatically install
+[[ -d ~/.config/nvim/ ]] || mkdir ~/.config/nvim
+[[ -f ~/.config/nvim/init.vim ]] || touch ~/.config/nvim/init.vim
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
+echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
 
 # download solarized
 wget -O $HOME/Downloads/solarized.zip "http://ethanschoonover.com/solarized/files/solarized.zip"
 
+# Final setup message:
 
+echo "
+You should be all set.  Manual steps remaining:
+1) Dropbox and 1password sync
+2) Change battery% display and dark theme 
+3) Setup terminal colors
+4) Install vim plugins
+"
