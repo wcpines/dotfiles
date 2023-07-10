aa869eae57c5ae6ad55b6dc13ecc1eb852065419
Colby Pines <wcpines@protonmail.com>
2023-07-10 11:37:53 -0700

    wip

--- vimrc.d/init.lua
+++ vimrc.d/init.lua
@@ -2,12 +2,23 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-	"eslint",
-	"lua_ls",
-	"sqlls",
-	"tsserver",
-})
+-- lsp.ensure_installed({
+-- 	"shellcheck",
+-- 	"codespell",
+-- 	"elixir-ls",
+-- 	"eslint",
+-- 	"eslint-lsp",
+-- 	"eslint_d",
+-- 	"lua-language-server",
+-- 	"lua_ls",
+-- 	"prettier",
+-- 	"prettierd",
+-- 	"shfmt",
+-- 	"sqlls",
+-- 	"stylua",
+-- 	"tsserver",
+-- 	"typescript-language-server",
+-- })
 
 lsp.set_sign_icons({
 	error = "✘",
@@ -29,8 +40,12 @@ lsp.setup()
 local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
+		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.code_actions.shellcheck,
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.diagnostics.yamllint,
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.formatting.stylua,
 	},
 })
